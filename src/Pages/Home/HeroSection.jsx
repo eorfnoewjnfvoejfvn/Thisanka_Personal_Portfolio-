39e768b2bdc5b9494915e9e29f40fa649bb7cdff
Thisanka <19tweeraddana@wakatipu.school.nz>
2023-07-27 21:04:56 +1200

    ceebs the hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,25 +1,24 @@
 export default function HeroSection() {
-    return (
-      <section id="heroSection" className="hero-section">
-        <div className="hero-section-content-box">
-          <div className="hero-section-content">
-            <p className="section-title">Kia Ora, I'm Thisanka</p>
-            <h1 className="hero-section--title">
-              <span className="hero-section-title--color">A Future</span>{" "}
-              <br />
-              Software engineer
-            </h1>
-            <p className="hero-section-description">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-              Maxime impedit ullam sapiente corrupti dolorem cum!
-            </p>
-          </div>
-          <button className="btn btn-primary">Contact me</button>
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Kia Ora I'm Thisanka</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">A Futre</span>{" "}
+            <br />
+            Software Engineer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          </p>
         </div>
-        <div className="hero-section--img">
-          <img src="./img/hero_img.png" alt="Hero Section" />
-        </div>
-      </section>
-    );
-  }
-
+        <button className="btn btn-primary">Contact me</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
