75d6fd48c614dac8630d9acb3553c18cfd3b9375
Thisanka <19tweeraddana@wakatipu.school.nz>
2023-08-03 20:04:08 +1200

    finished my skills css

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../HeroSection";
 import Navbar from "../Navbar";
+import MySkills from "../MySkills";
 
 export default function Home() {
     return (
       <>
         <Navbar/>
         <HeroSection />
+        <MySkills />
       </>
     );
   }
